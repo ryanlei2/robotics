24f8423df2078a152c3e3c86244c53a0e2c857b1
Ryan Lei <ryanlei726@gmail.com>
2022-12-05 17:43:59 -0800

    change icons please work

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,6 +70,7 @@ const config = {
             position: 'left',
             label: 'Introduction',
           },
+          //Uncomment this to see the Blog button on nav
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/ryanlei2/robotics',
