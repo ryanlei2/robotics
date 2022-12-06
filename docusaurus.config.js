6a7d649a941daa2652e54cff6c0872fb49d3b165
Ryan Lei <ryanlei726@gmail.com>
2022-12-05 19:25:59 -0800

    The best version (no bugs)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ryanlei2/robotics',
+            'https://github.com/ryanlei2/robotics/blob/main',
         },
         blog: {
           showReadingTime: true,
